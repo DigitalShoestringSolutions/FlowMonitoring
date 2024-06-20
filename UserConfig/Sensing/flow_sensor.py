e5b65cbe757fc86a7c610ce72dd75bcde854623e
Toby Harris <51968582+tobyaharris@users.noreply.github.com>
2024-06-20 17:59:22 +0100

    tore apart flow_dc

--- UserConfig/Sensing/flow_sensor.py
+++ UserConfig/Sensing/flow_sensor.py
@@ -1,7 +1,6 @@
 import RPi.GPIO as GPIO
 import time
 import logging
-import tomli
 
 # Setup logger
 logger = logging.getLogger('main.measure.sensor')
@@ -20,11 +19,6 @@ class WaterFlowSensor:
     def count_pulse(self, channel):
         self.count += 1
 
-    # def calculate_flow_rate(self):
-    #     flow_rate = (self.count / 4) / 60  # Assuming 7.5 pulses per liter
-    #     self.count = 0  # Reset pulse counter
-    #     return flow_rate
-
     def calculate_flow_rate(self):
         flow_rate = (self.count * 2.25 * 60) / 1000  # Assuming 7.5 pulses per liter...gives values in L/min
         self.count = 0  # Reset pulse counter
@@ -41,25 +35,5 @@ class WaterFlowSensor:
         GPIO.remove_event_detect(self.flow_pin)  # Remove event detection
         GPIO.cleanup(self.flow_pin)  # Clean up the specific pin
 
-# # def get_config():
-# #     with open("./config/config.toml", "rb") as f:
-# #         toml_conf = tomli.load(f)
-# #     logger.info(f"config: {toml_conf}")
-# #     return toml_conf
 
-# if __name__ == "__main__":
-#     # Basic configuration for logging
-#     logging.basicConfig(level=logging.INFO)
-    
-#     # config = get_config()
-#     # pin = config["connection"]["pin"]
-#     # pin = 17
-#     sensor = WaterFlowSensor()
-#     try:
-#         # while True:
-#         readings = sensor.read()
-#         logger.info(f"raw sensor vals: {readings}")
-#     # except KeyboardInterrupt:
-#     #     print("Interrupted by user")
-#     finally:
-#         sensor.cleanup()
+# payload = {"machine": self.constants['machine'], "source": self.constants['source'], "flow": str(average_reading), "sensor_type": pitch, "max_pressure": max_pressure, "min_flow": str(min_flow), "max_flow": str(max_flow), "timestamp": timestamp}
