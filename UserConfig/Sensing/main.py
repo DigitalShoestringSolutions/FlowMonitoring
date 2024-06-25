e33b0ab41ec9b36237fdbb859e9dcf4399c368a3
Toby Harris <51968582+tobyaharris@users.noreply.github.com>
2024-06-25 14:21:29 +0100

    Simplified MQTT messages and telegraf parsing

--- UserConfig/Sensing/main.py
+++ UserConfig/Sensing/main.py
@@ -8,16 +8,15 @@ from utilities.mqtt_out import publish
 # settings
 pin_number = 17
 pulses_per_litre = 7.5
-sensor_name = "Pipe1"
+machine = "pressure_washer"
+source = "overhead_tank"
 
 # setup sensors and models
 flow_sensor = PulseCounter(pin_number)
 
 # loop
 while True:
-    sleep(1)
-    flow_sensor_readings = flow_sensor(rounding=None)
-    print(flow_sensor_readings)
-    flow_rate = flow_sensor_readings["density"] * 60 / pulses_per_litre       # pulses per second to litres per minute
-    print(flow_rate)
-    publish( flow_sensor_readings | { "machine" : sensor_name, "flow_rate" : flow_rate })
+    sleep(3)
+    flow_rate = flow_sensor.get_recent_pulse_density() * 60 / pulses_per_litre       # pulses per second to litres per minute
+    publish({ "flow_rate" : flow_rate , "machine" : machine , "source" : source })
+    
