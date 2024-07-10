9ab5e71db6cec7610358fc7c39a7fcac72467313
Toby Harris <51968582+tobyaharris@users.noreply.github.com>
2024-07-10 10:55:35 +0100

    Centralised pulses_per_litre variable

--- UserConfig/Sensing/main.py
+++ UserConfig/Sensing/main.py
@@ -8,14 +8,15 @@ from utilities.mqtt_out import publish
 # setup sensors and models
 # I have a flow sensor connected to pin 17 (BCM) that outputs a 7.5 pulses per litre passed
 flow_sensor = PulseCounter(pin_num=17)
+pulses_per_litre = 7.5 
 
 # loop
 while True:
     sleep(3)
     pulses, density = flow_sensor.recent_pulses_and_density()
     
-    flow = pulses / 7.5             # pulses to litres
-    flow_rate = density*3600/7.5    # pulses per second to litres per hour
+    flow = pulses / pulses_per_litre             # pulses to litres
+    flow_rate = density*3600/pulses_per_litre    # pulses per second to litres per hour
  
     publish({ "flow": flow, "flow_rate" : flow_rate , "machine" : "MyMachineNameHere" , "source" : "MySourceNameHere" })
     
