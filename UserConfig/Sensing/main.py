bf9033753ce5fdceb5f7dc437dc3ad040ddbbd44
Toby Harris <51968582+tobyaharris@users.noreply.github.com>
2024-09-19 17:03:51 +0100

    Back to one Sensing module

--- UserConfig/Sensing/main.py
+++ UserConfig/Sensing/main.py
@@ -5,17 +5,20 @@ from time import sleep
 from hardware.generic.PulseCounter import PulseCounter
 from utilities.mqtt_out import publish
 
+
 # setup sensors and models.
-# I have a flow sensor connected to pin 26 (BCM) that outputs a 7.5 pulses per litre passed.
-flow_sensor = PulseCounter(pin_num=26)
-pulses_per_litre = 7.5
+# I have a flow sensor connected to pin 26 (BCM) that outputs 7.5 pulses per litre passed.
+flow_sensor_1 = PulseCounter(pin_num=26, multiplier=1/7.5)
+# I have another flow sensor connected to pin 19 (BCM) that outputs 20 pulses per litre passed.
+flow_sensor_2 = PulseCounter(pin_num=19, multiplier=1/20)
+
 
 # loop
 while True:
-    sleep(3)
+    sleep(3)    # Publish readings every 3 seconds
 
-    pulses, density = flow_sensor.recent_pulses_and_density()
-    flow = pulses / pulses_per_litre             # pulses to litres
-    flow_rate = density*3600/pulses_per_litre    # pulses per second to litres per hour
-
-    publish({ "flow": flow, "flow_rate" : flow_rate , "machine" : "MyMachineName1" , "source" : "MySourceName1" })
+    volume, rate = flow_sensor_1.recent_pulses_and_density()    # litres, litres per second
+    publish({ "flow": volume, "flow_rate" : rate , "machine" : "MyMachineName1" , "source" : "MySourceName1" })
+    
+    volume, rate = flow_sensor_2.recent_pulses_and_density()    # litres, litres per second
+    publish({ "flow": volume, "flow_rate" : rate , "machine" : "MyMachineName2" , "source" : "MySourceName2" })
